72f1eba8175b06c96f47ca31d335d2364e19b383
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-07-20 20:39:07 +0530

    commit message

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const statement = "Seamless Hiring Solutions at Your Fingertips!";
+const animationText = document.getElementById("animation-text");
+
+function animateText(text, index) {
+  if (index < text.length) {
+    animationText.textContent += text[index];
+    setTimeout(() => {
+      animateText(text, index + 1);
+    }, 100); // Adjust the delay (in milliseconds) between each character
+  }
+}
+
+
+
